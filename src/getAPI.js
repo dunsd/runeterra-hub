8d6d740c272aab51d6fe0cdfdcacb9ae0be64047
dunsd <dthomasparker@gmail.com>
2023-03-01 19:36:07 +0000

    Set up base files with webpack copied base layout from library project, deleted contents and add console log for base api fetch

---
+++ src/getAPI.js
@@ -0,0 +1,4 @@
+fetch("https://europe.api.riotgames.com/lor/match/v1/matches/by-puuid/OLAK4Fna1yAyLqMFTJ1gPqICrCGwWEZPSQs9JdWtGtzz1NJezSSH8QMgF2jrtcmk0u5fV7lyPl8Uvw/ids?api_key=RGAPI-214a8f70-95a8-4018-a3c7-3dd2a6c0ac49")
+.then((response) => response.json())
+.then((data) => console.log(data));
+  
